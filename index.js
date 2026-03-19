cd9c19c2ece823d9be23c58b65e69dc459ae0ca5
Dhruv Thakur <tdhruv693@gmail.com>
2026-03-19 14:01:31 +0530

    added postgreSQL instead of using array for storage

--- index.js
+++ index.js
@@ -1,90 +1,88 @@
+require("dotenv").config();
+const pool=require("./db");
 const express=require('express');
 const app=express();
 const port=3000;
-const { Pool }=require("pg");
-let notes=[];
-let idcount=0;
 app.use(express.json());
 
 app.get("/",(req,res)=>{
     res.send("hello welcome to my node server");
 });
 
-app.get("/notes",(req,res)=>{
-    res.json(notes);
+app.get("/notes",async (req,res)=>{
+    const data=await pool.query("SELECT * FROM NOTES");
+    res.json(data.rows);
 })
-app.post("/notes",(req,res)=>{
+app.post("/notes",async (req,res)=>{
     if(!req.body.title || !req.body.content){
-        res.status(400).json({
+        return res.status(400).json({
             message:"values not specified"
         })
     }
-    let newNote={
-        id:idcount++,
-        title:req.body.title,
-        content:req.body.content,
-        tags:req.body.tags
-    }
-    notes.push(newNote);
-    res.json(newNote);
+    const data=await pool.query("INSERT INTO NOTES (title,content) VALUES ($1,$2) RETURNING *",[req.body.title,req.body.content]);
+    res.json(data.rows[0]);
 })
 
-app.get("/notes/:id",(req,res)=>{
+app.get("/notes/:id",async (req,res)=>{
     let ids=req.params.id;
     if(!Number(ids)){
-        res.status(400).json({
+        return res.status(400).json({
             message:"wrong id specified, bad manners"
         })
     }
     ids=Number(ids);
-    let obj=notes.find(o=>o.id===ids);
-    if(!obj){
-        res.status(404).json({
-            message:"no note found!"
-        });
-    }
-    res.json(obj);
-})
-
-app.delete("/notes/:id",(req,res)=>{
-    let ids=req.params.id;
-    ids=Number(ids);
-    if(!ids){
-        res.status(400).json({
+    const result=await pool.query("SELECT * FROM NOTES WHERE ID=$1",[ids]);
+    if(result.rows.length===0){
+        return res.status(404).json({
             message:"invalid id"
         })
     }
-    let obj=notes.find(o=>o.id===ids);
-    if(!obj){
-        res.status(404).json({
-            message:"No note found!"
-        })
-    }
-    notes=notes.filter(o=>o.id===!ids);
-    res.json({
-        message:"successfully deleted",
-        deletednote:obj
-    })
+    res.json(result.rows);
 })
 
-app.put("/notes/:id",(req,res)=>{
+app.delete("/notes/:id",async (req,res)=>{
     let ids=req.params.id;
     ids=Number(ids);
     if(!ids){
-        res.status(400).json({
+        return res.status(400).json({
+            message:"invalid id"
+        })
+    }
+    const result=await pool.query("DELETE FROM NOTES WHERE ID=$1 RETURNING *",[ids]);
+    if(result.rows.length===0){
+        return res.status(404).json({
+            message:"no related id's found"
+        })
+    }
+    res.json({
+        message:"successfully deleted",
+        deleted:result.rows[0]
+    })
+})
+
+app.put("/notes/:id",async (req,res)=>{
+    let ids=req.params.id;
+    ids=Number(ids);
+    if(!ids){
+        return res.status(400).json({
             message:"Invalid id"
         })
     }
-    let note=notes.find(o=>o.id===ids);
     if(req.body.title){
-        note.title=req.body.title;
+        await pool.query("UPDATE NOTES SET title=$1 WHERE ID=$2",[req.body.title,ids]);
     }
     if(req.body.content){
-        note.content=req.body.content;
+        await pool.query("UPDATE NOTES SET content=$1 WHERE ID=$2",[req.body.content,ids]);
+    }
+    const result=await pool.query("SELECT * FROM NOTES WHERE ID=$1",[ids]);
+    if(result.rows.length===0){
+        return res.status(404).json({
+            message:"no id found"
+        })
     }
     res.json({
         message:"successfully changed",
-        updatednote:note
+        updatednote:result.rows
     })
 })
 app.listen(port,()=>{
